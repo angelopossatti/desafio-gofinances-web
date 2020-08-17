21ee48d46dc94ffbebd4e21bbaf2d32cca4a3e25
Angelo Marzari Possatti <angelopossatti@gmail.com>
2020-08-16 22:16:51 -0300

    Created web app to upload files csv.

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -13,11 +13,10 @@ interface HeaderProps {
 const Header: React.FC<HeaderProps> = ({ size = 'large' }: HeaderProps) => (
   <Container size={size}>
     <header>
-      <img src={Logo} alt="GoFinances" />
+      <img src={Logo} alt='GoFinances' />
       <nav>
-        {
-          // Todo
-        }
+          <Link to='/'>Listagem</Link>
+          <Link to='/import'>Importar</Link>
       </nav>
     </header>
   </Container>
